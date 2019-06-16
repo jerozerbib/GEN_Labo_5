3e21cfd797077a1c39e26a40f791e037d3ca96f9
unknown <batach31@hotmail.com>
2019-06-16 19:46:46 +0200

    uncommented main

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,7 @@
 #include "Customer.h"
 
 int main(int argc, char **argv) {
-
-    /*::testing::InitGoogleMock(&argc, argv);
+    ::testing::InitGoogleMock(&argc, argv);
     ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();*/
+    return RUN_ALL_TESTS();
 }
